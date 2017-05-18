f56ad779494b3e61123ba234ba9f06931c4eb1d5
Kavish <kavishrmunjal@gmail.com>
2017-05-17 19:15:54 -0500

    Added option to randomize the board. Implemented DFS and BFS solvers

--- EightPuzzleSolver/MainWindow.xaml.cs
+++ EightPuzzleSolver/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace EightPuzzleSolver
     public partial class MainWindow : Window
     {
         private int[,] board = new int[,] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
+        enum algorithms { dfs, bfs, gbfs, astar};
+        algorithms algorithmUsed = algorithms.dfs;
 
         public MainWindow()
         {
@@ -74,40 +76,153 @@ namespace EightPuzzleSolver
 
         private void swapWithtEmptyNeighbor(int rowNum, int colNum)
         {
-            Button currentButton = getButton(rowNum, colNum) as Button;
-            Button topElement = getButton(rowNum - 1, colNum) as Button;
-            Button leftElement = getButton(rowNum, colNum - 1) as Button;
-            Button bottomElement = getButton(rowNum + 1, colNum) as Button;
-            Button rightElement = getButton(rowNum, colNum + 1) as Button;
+            Button currentButton = getButton(rowNum, colNum);
 
-            trySwapButtonContents(currentButton, topElement);
-            trySwapButtonContents(currentButton, leftElement);
-            trySwapButtonContents(currentButton, bottomElement);
-            trySwapButtonContents(currentButton, rightElement);
+            trySwapButtonContents(currentButton, getButton(rowNum - 1, colNum));
+            trySwapButtonContents(currentButton, getButton(rowNum, colNum - 1));
+            trySwapButtonContents(currentButton, getButton(rowNum + 1, colNum));
+            trySwapButtonContents(currentButton, getButton(rowNum, colNum + 1));
         }
 
-        private void trySwapButtonContents(Button button1, Button button2)
+        private bool trySwapButtonContents(Button button1, Button button2)
         {
             if (button1 == null || button2 == null)
             {
-                return;
+                return false;
             }
 
             if (0 == String.Compare(button2.Content.ToString(), ""))
             {
                 button2.Content = button1.Content;
                 button1.Content = "";
+                return true;
             }
+
+            return false;
         }
 
-        private UIElement getButton(int rowNum, int colNum)
+        private Button getButton(int rowNum, int colNum)
         {
             if (rowNum < 1 || colNum < 0 || rowNum > 3 || colNum > 2)
             {
                 return null;
             }
 
-            return GameGrid.Children.Cast<UIElement>().First(i => Grid.GetRow(i) == rowNum && Grid.GetColumn(i) == colNum);
+            return GameGrid.Children.Cast<UIElement>().First(i => Grid.GetRow(i) == rowNum && Grid.GetColumn(i) == colNum) as Button;
+        }
+
+        private void randomize_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 20000; i++)
+            {
+                randomMove();
+            }
+        }
+
+        private void randomMove()
+        {
+            Button emptyButton = null;
+            int emptyButtonRow = -1, emptyButtonColumn = -1;
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    emptyButton = getButton(i, j);
+                    if (String.IsNullOrEmpty(emptyButton.Content.ToString()))
+                    {
+                        emptyButtonRow = i;
+                        emptyButtonColumn = j;
+                        break;
+                    }
+                }
+                if (emptyButtonRow != -1)
+                {
+                    break;
+                }
+            }
+
+            bool swapSuccess = false;
+            Random rnd = new Random();
+
+            while (!swapSuccess)
+            {
+                int rando = rnd.Next(4);
+                switch (rando)
+                {
+                    case (0):
+                        swapSuccess = trySwapButtonContents(getButton(emptyButtonRow - 1, emptyButtonColumn), emptyButton);
+                        break;
+                    case (1):
+                        swapSuccess = trySwapButtonContents(getButton(emptyButtonRow, emptyButtonColumn - 1), emptyButton);
+                        break;
+                    case (2):
+                        swapSuccess = trySwapButtonContents(getButton(emptyButtonRow + 1, emptyButtonColumn), emptyButton);
+                        break;
+                    case (3):
+                        swapSuccess = trySwapButtonContents(getButton(emptyButtonRow, emptyButtonColumn + 1), emptyButton);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Algorithm_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (sender as RadioButton);
+            if (0 == String.Compare(rb.Name, "DFS"))
+            {
+                algorithmUsed = algorithms.dfs;
+            }
+            else if (0 == String.Compare(rb.Name, "BFS"))
+            {
+                algorithmUsed = algorithms.bfs;
+            }
+            else if(0 == String.Compare(rb.Name, "GBFS"))
+            {
+                algorithmUsed = algorithms.gbfs;
+            }
+            else
+            {
+                algorithmUsed = algorithms.astar;
+            }
+        }
+
+        private void solve_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    string buttonContent = getButton(i, j).Content.ToString();
+                    if (String.IsNullOrEmpty(buttonContent))
+                        board[i - 1, j] = 0;
+                    else
+                        board[i - 1, j] = int.Parse(buttonContent);
+                }
+            }
+
+            boardSolver BoardSolver = new boardSolver(this.board);
+            int nSteps;
+
+            switch (algorithmUsed)
+            {
+                case (algorithms.dfs):
+                    nSteps = BoardSolver.depthFirstSearch();
+                    break;
+                case (algorithms.bfs):
+                    nSteps = BoardSolver.breadthFirstSearch();
+                    break;
+                case (algorithms.gbfs):
+                    break;
+                case (algorithms.astar):
+                    break;
+            }
+        }
+
+        private bool solved (int[,] currentBoard)
+        {
+            return false;
         }
     }
 }
---
+++ EightPuzzleSolver/boardSolver.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzleSolver
+{
+    class boardSolver
+    {
+        enum neighbors { right, bottom, left, top };
+
+        private int[,] currentBoard;
+        private int[,] solvedBoard = new int[,] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
+
+        HashSet<string> boardsSeen;
+        Dictionary<string, string> parentMap;
+
+        public boardSolver(int[,] board)
+        {
+            this.currentBoard = board;
+
+            boardsSeen = new HashSet<string>();
+            parentMap = new Dictionary<string, string>();
+
+            boardsSeen.Add(boardToString(board));
+            parentMap.Add(boardToString(solvedBoard), boardToString(solvedBoard));
+        }
+
+        public int depthFirstSearch()
+        {
+            Stack<int[,]> boardStack = new Stack<int[,]>();
+            boardStack.Push(currentBoard);
+            int[,] boardCheck = boardStack.Pop();
+            int nSteps = 0;
+
+            while (!isSolved(boardCheck))
+            {
+                foreach (neighbors neighbor in Enum.GetValues(typeof(neighbors)))
+                {
+                    int[,] neighborBoard = getNeighbor(boardCheck, neighbor);
+                    if (neighborBoard != null && !boardsSeen.Contains(boardToString(neighborBoard)))
+                    {
+                        boardStack.Push(neighborBoard);
+                        boardsSeen.Add(boardToString(neighborBoard));
+                        parentMap[boardToString(neighborBoard)] = boardToString(boardCheck);
+                        if (isSolved(neighborBoard))
+                        {
+                            nSteps++;
+                            goto solved;
+                        }
+                    }
+                }
+                nSteps++;
+
+                boardCheck = boardStack.Pop();
+            }
+
+            solved:
+            string originalBoard = boardToString(this.currentBoard);
+            string childBoard = parentMap[boardToString(solvedBoard)];
+            int nRealSteps = 0;
+            while (childBoard != originalBoard)
+            {
+                childBoard = parentMap[childBoard];
+                nRealSteps++;
+            }
+
+            return nRealSteps;
+        }
+
+        public int breadthFirstSearch()
+        {
+            Queue<int[,]> boardQueue = new Queue<int[,]>();
+            boardQueue.Enqueue(currentBoard);
+            int[,] boardCheck = boardQueue.Dequeue();
+            int nSteps = 0;
+
+            while (!isSolved(boardCheck))
+            {
+                foreach (neighbors neighbor in Enum.GetValues(typeof(neighbors)))
+                {
+                    int[,] neighborBoard = getNeighbor(boardCheck, neighbor);
+                    if (neighborBoard != null && !boardsSeen.Contains(boardToString(neighborBoard)))
+                    {
+                        boardQueue.Enqueue(neighborBoard);
+                        boardsSeen.Add(boardToString(neighborBoard));
+                        parentMap[boardToString(neighborBoard)] = boardToString(boardCheck);
+                        if (isSolved(neighborBoard))
+                        {
+                            nSteps++;
+                            goto solved;
+                        }
+                    }
+                }
+                nSteps++;
+
+                boardCheck = boardQueue.Dequeue();
+            }
+
+            solved:
+            string originalBoard = boardToString(this.currentBoard);
+            string childBoard = parentMap[boardToString(solvedBoard)];
+            int nRealSteps = 0;
+            while (childBoard != originalBoard)
+            {
+                childBoard = parentMap[childBoard];
+                nRealSteps++;
+            }
+
+            return nRealSteps;
+        }
+
+        private bool isSolved(int[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] != solvedBoard[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private int[,] getNeighbor(int[,] board, neighbors neighbor)
+        {
+            int[,] returnBoard = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            int emptyX = -1, emptyY = -1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    returnBoard[i, j] = board[i, j];
+                    if (board[i, j] == 0)
+                    {
+                        emptyX = i;
+                        emptyY = j;
+                    }
+                }
+            }
+
+            switch (neighbor)
+            {
+                case (neighbors.top):
+                    if (emptyX > 0)
+                    {
+                        returnBoard[emptyX, emptyY] = board[emptyX - 1, emptyY];
+                        returnBoard[emptyX - 1, emptyY] = board[emptyX, emptyY];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    break;
+
+                case (neighbors.left):
+                    if (emptyY > 0)
+                    {
+                        returnBoard[emptyX, emptyY] = board[emptyX, emptyY - 1];
+                        returnBoard[emptyX, emptyY - 1] = board[emptyX, emptyY];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    break;
+
+                case (neighbors.bottom):
+                    if (emptyX < 2)
+                    {
+                        returnBoard[emptyX, emptyY] = board[emptyX + 1, emptyY];
+                        returnBoard[emptyX + 1, emptyY] = board[emptyX, emptyY];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    break;
+
+                case (neighbors.right):
+                    if (emptyY < 2)
+                    {
+                        returnBoard[emptyX, emptyY] = board[emptyX, emptyY + 1];
+                        returnBoard[emptyX, emptyY + 1] = board[emptyX, emptyY];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    break;
+            }
+
+            return returnBoard;
+        }
+
+        private string boardToString(int[,] board)
+        {
+            string returnString = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    returnString += board[i, j].ToString();
+                }
+            }
+
+            return returnString;
+        }
+    }
+}
